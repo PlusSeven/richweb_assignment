28dabe9c9b1a6b211f489c9c3442460b77e78341
plusseven <plus7.jiang@gmail.com>
2013-04-16 17:18:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RichwebAssignment::Application.config.secret_token = '73f0f4b433d8d6334d0641e52811660278e79afc91bdf00bcde45aec2086d081f8d53ec1b149d9e0ed578aa4f97d35b29245f6f0e2e0c8edb013821796f2dd35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RichwebAssignment::Application.config.session_store :cookie_store, key: '_richweb_assignment_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RichwebAssignment::Application.config.session_store :active_record_store
